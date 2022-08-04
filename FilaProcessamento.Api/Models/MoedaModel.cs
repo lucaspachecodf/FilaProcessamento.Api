203ccf3cbedd8951f092edfd3c484058cf3beb86
Lucas dos Santos Pacheco <lucaspachecodf@gmail.com>
2022-08-04 10:31:50 -0300

    Formato da data json

--- FilaProcessamento.Api/Models/MoedaModel.cs
+++ FilaProcessamento.Api/Models/MoedaModel.cs
@@ -1,13 +1,19 @@
 ﻿using FilaProcessamento.Domain.Validations.Contract;
 using FilaProcessamento.Domain.Validations.Notifications;
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace FilaProcessamento.Api.Models
 {
     public class MoedaModel : Notifiable, IValidatable
     {
         public string Moeda { get; set; }
+        [DataType(DataType.Date)]
+        [JsonPropertyName("data_inicio")]
         public DateTime DataInicio { get; set; }
+        [DataType(DataType.Date)]
+        [JsonPropertyName("data_fim")]
         public DateTime DataFim { get; set; }
 
         public void Validate()
